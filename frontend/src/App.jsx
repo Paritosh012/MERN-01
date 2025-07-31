c35b44588d58e39a08d5acfea777793d5e252186
unknown <Paritosh Jaiswal>
2025-07-31 18:26:25 +0530

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,40 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Layout from "./Layout";
+import Home from "./pages/Home";
+import Insert from "./pages/Insert";
+import Display from "./pages/Display";
+import Update from "./pages/Update";
+import Search from "./pages/Search";
+import Contact from "./pages/Contact";
+
+const App = () => {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="home" element={<Home />} />
+            <Route path="insert" element={<Insert />} />
+            <Route path="display" element={<Display />} />
+            <Route path="update" element={<Update />} />
+            <Route path="search" element={<Search />} />
+            <Route path="contact" element={<Contact />} />
+            <Route
+              path="*"
+              element={
+                <>
+                  <h1 align="center" color="red">
+                    Error 404 Not Found
+                  </h1>
+                </>
+              }
+            />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+};
+
+export default App;
---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,10 @@
+import  "../css/Footer.css"
+
+const Footer = () => {
+  return (
+    <>
+      <div id="footer"></div>
+    </>
+  );
+};
+export default Footer;
