ab3d7cd71636f1ee7acdcd40e8e0c7efa28664f5
unknown <Paritosh Jaiswal>
2025-08-07 15:38:54 +0530

    Fixed backend routes to include /students prefix

--- backend/route/stuRoute.js
+++ backend/route/stuRoute.js
@@ -6,16 +6,16 @@ const route = express.Router();
 route.get("/", stuController.homePage);
 route.get("/students", stuController.homePage);
 
-route.post("/save", stuController.stuSave);
+route.post("/students/save", stuController.stuSave);
 
-route.get("/display", stuController.display);
-route.get("/update", stuController.update);
-route.get("/editData/:id", stuController.editData);
+route.get("/students/display", stuController.display);
+route.get("/students/update", stuController.update);
+route.get("/students/editData/:id", stuController.editData);
 
-route.put("/updateData/:id", stuController.updateData);
+route.put("/students/updateData/:id", stuController.updateData);
 
-route.delete("/deleteData/:id", stuController.deleteData);
+route.delete("/students/deleteData/:id", stuController.deleteData);
 
-route.post("/search",stuController.searchData)
+route.post("/students/search",stuController.searchData)
 
 module.exports = route;
